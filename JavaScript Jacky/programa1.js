ce44c9c08e11dbbf807383935081d411dd2ff70d
JaquelineLopez <lopezalvarezjaqueline7@gmail.com>
2026-03-10 19:11:41 -0600

    Add files via upload

---
+++ JavaScript Jacky/programa1.js
@@ -0,0 +1,19 @@
+//var //que la variable tenga un ambito mas general
+//let //ambito de bloque
+//const //se usa para elementos que no cambian su valor en tiempo de ejecucion es decir son constantes
+
+//operador terario: condicion: valor_si_Verdadero : valor_si_falso
+/*const age = 18;
+const status = age >= 18 ? "Adult" : "Minor";
+console.log(status);*/
+
+//casting convertir o forzar un tipo de dato a otro
+
+const temp = 25;
+if (temp > 30) {
+    console.log("It's hot.");
+} else if (temp >= 20) {
+    console.log("It's warm.");
+} else {
+    console.log("It's cold.");
+}
